4ca30bac742e17fe619ebffe0ef6ab2827d874f2
Troy Ciesco <tmciesco@gmail.com>
2020-05-27 18:12:07 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-snipcart`,
+      options: {
+        apiKey:
+          "ZDg5Njg3ODItNTEyYS00NzMxLTk3YmItYWY3NzVjMDhmYTA1NjM3MDI4NzczNDI0Mzg3OTU2",
+        js: `https://cdn.snipcart.com/themes/v3.0.14/default/snipcart.js`,
+        jquery: false,
+        styles: "https://cdn.snipcart.com/themes/v3.0.14/default/snipcart.css",
+      },
+    },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: "3cc5a4626eb9adf07934226e6d5645",
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+export const query = graphql`
+  query MyQuery {
+    datoCmsSite {
+      globalSeo {
+        facebookPageUrl
+        siteName
+        titleSuffix
+        twitterAccount
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to {data.datoCmsSite.globalSeo.siteName}.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <section>
+      <h2>Silver Stacking Ring</h2>
+      <p>$19.99</p>
+      <p>Wear one or seventeen! These rings are fun to mix and match.</p>
+      <button
+        className="snipcart-add-item snipcart-checkout"
+        data-item-id="silver-stacking-ring"
+        data-item-price="19.99"
+        data-item-url="/"
+        data-item-name="Silver Stacking Ring"
+      >
+        Add to cart
+      </button>
+    </section>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
